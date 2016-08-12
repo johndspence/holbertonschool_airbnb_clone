e02ae63d3e4e2edbb021f48fa1a57cb60814bf32
John Spence <johnspence@Macbook.local>
2016-08-11 22:33:38 -0700

    Replacing entire code set with local copy

--- api/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from flask_json import FlaskJSON
-
-app = Flask(__name__)
-json = FlaskJSON(app)
--- api/app/views/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["index", "user", "state", "city", "place", "place_book", "amenity"]
